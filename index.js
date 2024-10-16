5b1f60aa5348418c047fc2bdc29c9c9567370473
Amitkumarc78f <amitc78f@gmail.com>
2024-10-17 05:20:56 +0530

    done

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ connectDB();
 app.use(bodyParser.json());
 
 app.use(cors({
-  origin:process.env.FRONT_END_URL || "http://localhost:5173",
+  origin:process.env.FRONT_END_URL || "https://frontend1-firg.onrender.com/",
   methods:['GET','POST','PUT','DELETE'],
   credentials:true
 }))
